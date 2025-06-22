38995863b3498adb0fe5e378dda915dcb4afd70e
esdmr <esdmr0@gmail.com>
2025-06-22 22:21:30 +0330

    Setup nginx, anubis, prometheus and grafana

--- tls/server/update-dlink-ip.sh
+++ tls/server/update-dlink-ip.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit
 
-printf 'subjectAltName=DNS:esdmr.iddns.ir,IP:%s\n' "$(../../dnslab/get-dlink-ip.sh)" >ext.cnf
+printf 'subjectAltName=DNS:esdmr.iddns.ir,DNS:localhost,IP:127.0.0.1,IP:192.168.1.2,IP:%s\n' "$(../../dnslab/get-dlink-ip.sh)" >ext.cnf
