804dd0d23602be43c88ccc9c0403e1da94501a4f
esdmr <esdmr0@gmail.com>
2025-06-23 15:44:19 +0330

    Combine get-dlink-ip and update-dlink-ip

---
+++ dlink.update-name.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cd "$(dirname "$0")" || exit
+
+ip="$(expect -c "
+	spawn telnet 192.168.1.1
+	expect \"Login\"
+	send \"admin\r\"
+	expect \"Password\"
+	send \"$(cat dlink.key.txt)\\r\"
+	expect \">\"
+	send \"ifconfig\\r\"
+	expect \">\"
+	send \"exit\\r\"
+	interact
+" | grep -o -P '(?<=inet addr:)(?!192|127)\d+\.\d+\.\d+\.\d+')"
+
+printf 'subjectAltName=DNS:esdmr.iddns.ir,DNS:localhost,IP:127.0.0.1,IP:192.168.1.2,IP:%s' "$ip" >tls/server/ext.cnf
+
+printf 'server_name esdmr.iddns.ir localhost 127.0.0.1 192.168.1.2 %s "";\n' "$ip" >nginx/name.inc
