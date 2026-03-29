f5ed08067d1357b2484dd55885d93ceebb97a193
esdmr <esdmr0@gmail.com>
2026-03-29 23:28:27 +0330

    Configure anubis env vars

--- dlink.update-name.sh
+++ dlink.update-name.sh
@@ -18,6 +18,6 @@ domain=esdmr.iddns.ir
 
 echo $domain : $ip
 
-printf 'subjectAltName=DNS:%s,DNS:localhost,IP:127.0.0.1,IP:192.168.1.2,IP:%s' "$domain" "$ip" >tls/server/ext.cnf
+printf 'subjectAltName=DNS:%s,IP:127.0.0.1,IP:192.168.1.2,IP:%s' "$domain" "$ip" >tls/server/ext.cnf
 
-printf 'server_name %s localhost 127.0.0.1 192.168.1.2 %s "";\n' "$domain" "$ip" >nginx/name.inc
+printf 'server_name %s 127.0.0.1 192.168.1.2 %s "";\n' "$domain" "$ip" >nginx/name.inc
